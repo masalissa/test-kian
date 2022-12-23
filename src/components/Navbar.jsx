db50fd0c6d66dd24912cbfb3e1f69084c9840b55
mohammed-webdev <mas.webdev24@gmail.com>
2022-12-23 23:15:01 +0100

    change place of font folder from public to src

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {useLocation,NavLink} from "react-router-dom";
-import logo from "../images/logo-kiAn.png"
+import kiAnLogo from "../images/logo-kiAn.png"
 export default function Navbar() {
     const links = [
         {key:"films",value:"films"},
@@ -14,7 +14,7 @@ export default function Navbar() {
   return (
     <nav className={`navbar navbar-expand-md navbar-dark ${pathname !== "/" ? "background-black" :"position-absolute center-logo-class"} py-3`}>
     <div className={`${pathname !== "/" ? "container-fluid d-flex flex-md-column gap-md-3" : "container-fluid justify-content-center"}`}>
-      <a className="navbar-brand" href="/"><img src={logo} alt="logo" /></a>
+      <a className="navbar-brand" href="/"><img src={kiAnLogo} alt="Logo" /></a>
       {pathname !== "/" ? <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon" />
       </button> : null}
